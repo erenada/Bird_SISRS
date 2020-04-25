37546f737fec7939180ba6fc8d25f9b4bd4754d1
Eren Ada <erenada@gmail.com>
2020-04-24 20:49:59 -0400

    fix

--- QCReports/test.sh
+++ QCReports/test.sh
@@ -5,5 +5,5 @@ file="$1"
 while read line;
 do
     mkdir "${line}"
-    cp /data/schwartzlab/Trimmed_SISRS/Birds/fastQC/HTML/"${line}".html ./"${line}"
+    cp /data/schwartzlab/Trimmed_SISRS/Birds/fastQC/HTML/"${line}"*html ./"${line}"
 done
