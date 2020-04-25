966de56e67a799da84cc6d23fe16c1b5c0a1f6b1
Eren <erenada@gmail.com>
2020-04-24 21:05:10 -0400

    copyscript

---
+++ QCReports/test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+file="$1"
+
+for i in {1..48}
+
+do
+  read line
+  mkdir /data/schwartzlab/eren/Bird_SISRS/QCReports
+  cp /data/schwartzlab/Trimmed_SISRS/Birds/fastQC/HTML/"${line}"*html /data/schwartzlab/eren/Bird_SISRS/QCReports/${line}
+done < "$1"
