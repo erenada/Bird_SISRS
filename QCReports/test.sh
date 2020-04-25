69c79d7a3813804e4f1234b987e5edbecd713284
Eren <erenada@gmail.com>
2020-04-24 21:07:20 -0400

    Update test.sh

--- QCReports/test.sh
+++ QCReports/test.sh
@@ -6,6 +6,6 @@ for i in {1..48}
 
 do
   read line
-  mkdir /data/schwartzlab/eren/Bird_SISRS/QCReports
+  mkdir /data/schwartzlab/eren/Bird_SISRS/QCReports/"${line}"
   cp /data/schwartzlab/Trimmed_SISRS/Birds/fastQC/HTML/"${line}"*html /data/schwartzlab/eren/Bird_SISRS/QCReports/${line}
 done < "$1"
